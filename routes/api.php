114b2e1b206e81a23705a55ee3eaed2ac4c4d93b
denusklo <denusklo@gmail.com>
2021-12-30 16:07:35 +0800

    commit

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', function () {
+    return 'this is an API';
+});
+
 Route::get('/person', function () {
     $person = [
         'first_name' => 'Sean',
