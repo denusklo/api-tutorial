d895991265db99b7927f5bd29653e48879e82ae8
denusklo <denusklo@gmail.com>
2022-01-02 23:56:04 +0800

    commit

--- api/person.php
+++ api/person.php
@@ -1,7 +1,6 @@
 <?php
-require __DIR__ . '/../app/Providers/RouteServiceProvider.php';
 $person = [
     'first_name' => 'Sean',
     'last_name' => 'Pooley',
 ];
-echo ($person);
+echo (json_encode($person));
